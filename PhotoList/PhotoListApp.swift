30151a4ae2fa21de45e60b9449be9140c6c9a4a6
oznurkose <oznurkose01@gmail.com>
2023-04-07 13:25:46 +0300

    Initial Commit

---
+++ PhotoList/PhotoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoListApp.swift
+//  PhotoList
+//
+//  Created by Öznur Köse on 7.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
